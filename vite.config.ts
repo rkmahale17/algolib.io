6654461b2763887f982642a5df31811149ab8e75
Rahul Mahale <rahulmahale172214@gmail.com>
2025-10-10 01:03:17 +0530

    feat(optimization): optimising build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import { componentTagger } from "lovable-tagger";
+import compression from 'vite-plugin-compression';
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,7 +10,16 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(),
+
+  compression({
+    algorithm: 'brotliCompress', // Add Brotli compression
+    ext: '.br',
+    threshold: 10240,
+    deleteOriginFile: false,
+  }),
+
+  mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
