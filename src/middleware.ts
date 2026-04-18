d0487cba5e6fda6602fe50c0474614088f538999
Rahul Mahale <rahulmahale172214@gmail.com>
2026-04-18 15:09:24 +0530

    false commit for vercel

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,6 +13,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
+     * false comment
      */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
